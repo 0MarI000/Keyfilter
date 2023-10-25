b2faefc4ce41893bc9441e8e5a27180b524a7bec
0MarI000 <marekdv4@gmail.com>
2023-10-25 19:07:19 +0200

    provedl zmeny

--- keyfilter.c
+++ keyfilter.c
@@ -1,23 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
+
+
+
+//definuji si maximalni delku adresy
+#define MAX_LEN (100 * 42) + 1
+
+//vytvorim si funkci, ktera bude ziskavat hodnoty ze seznamu adres
+void read_ad(char** str, int* len)
+{
+char adr[MAX_LEN];
+char c;
+int i = 0;
+    while ((c = getchar()) != EOF)
+        {
+            adr[i] = c;
+            i++;
+        }
+    *str = &adr[0];
+    *len = i;
+}
+
+
+int main(int argc, char **argv)
 {
     if (argc < 2)
     {
-        fprintf(stderr, "Moc malo argumentu! \n");
+        fprintf(stderr, "Pocatecni pismena adresy nebyla zadana/nalezena\n");
         return 1;
     }
+    char* adr;
+    int len;
+    read_ad(&adr, &len);
     int i = 0;
-    int c;
-
-    printf("Zadejte znak k vyfiltrování: \n");
-    c = getchar();
-    while (argv[1][i] != '\0'){
-        if (argv[1][i] != c){
-            putchar(argv[1][i]);
+    int j = 0;
+    char curr = adr[i];
+    while(i < MAX_LEN && i < len)
+    {
+       
+        if(curr == argv[1][j])
+        {
+            printf("%c", curr);
         }
+        /*else
+        {
+            printf("zadana pismena se neobjevuji v adrese z listu");
+            break;
+        } 
+        */
+        //printf("%c", curr);
         i++;
+        j++;
     }
-    printf("\n");
+
+    //printf("\n%c", argv[1][0]);
     return 0;
 }
